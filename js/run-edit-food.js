fb25f200c171db1c115b4f74f85c63f72acee195
Kieran-Go <187461911+Kieran-Go@users.noreply.github.com>
2024-12-26 18:19:06 +1030

    Refactor JS code.
    Code has been refactored to multiple js files for easier
    management.
    An achor links to a new html page with no content. This page
    will be where the user can edit the food item list.

---
+++ js/run-edit-food.js
@@ -0,0 +1,1 @@
+let foodItems = loadFromLocalStorage("foodItems");
---
+++ js/run-index.js
@@ -0,0 +1,56 @@
+let foodItems = loadFromLocalStorage("foodItems") || [
+    new Food("Uncle Toby's Oats", 40, 153, 5.1, 22.7, 2),
+    new Food("Mixed Frozen Berries", 150, 61, 1.3, 10.3, 5),
+    new Food("Lite Milk", 250, 111, 8.8, 11.5, 110)
+];
+sortFoodSelector(); // Sort the array alphabetically
+
+// Initialize the 'eaten foods' array and table
+let eatenFoods = loadFromLocalStorage('eatenFoods') || [];
+populateTable();
+
+// Initialize the totals
+let totalCals = 0;
+let totalProtein = 0;
+let totalCarbs = 0;
+let totalSodium = 0;
+calcTotals();
+
+// Initialize the food selector
+const foodSelector = document.getElementById("food-selector");
+populateFoodSelector();
+foodSelector.addEventListener("change", () =>{
+    const defaultText = document.getElementById("amount-default");
+    const amount = document.querySelector(".amount");
+    let serveSize = 0;
+    for(let i = 0; i < foodItems.length; i++){
+        if(foodSelector.value === foodItems[i].name){
+            serveSize = foodItems[i].serveSize;
+            break;
+        }
+    }
+    defaultText.textContent = `1 serving = ${serveSize} g`;
+    amount.value = serveSize;
+});
+
+const addBtn = document.querySelector(".btn-add-food");
+addBtn.addEventListener("click", () =>{
+    const amount = document.querySelector(".amount").value;
+    if(amount != "" && !isNaN(amount) && amount > 0)
+    for(let i = 0; i < foodItems.length; i++){
+        const food = foodItems[i];
+        if(foodSelector.value === foodItems[i].name){
+            addFood(food, amount);
+            populateTable();
+            break;
+        }
+    }
+});
+
+const clearBtn = document.querySelector(".btn-clear-table");
+clearBtn.addEventListener("click", ()=>{
+    eatenFoods.splice(0, eatenFoods.length);
+    saveToLocalStorage('eatenFoods', eatenFoods);
+    populateTable();
+    calcTotals();
+});
